c05e848ec59f83f38a124fe6035440c6d0010c64
TemDPCTicxar <jhonatan.rodriguez@ticxar.com>
2024-11-21 23:39:25 -0500

    nuevo codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, 
             AuthModule,
-            SharedModule
+            SharedModule,
+            NgIf
         ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'oraculo';
+
+  constructor(private router: Router) {}
+
+  isLoginOrRegister(): boolean {
+    const currentRoute = this.router.url; // Obtener la ruta actual
+    return currentRoute === '/login' || currentRoute === '/register'; // Comparar con las rutas deseadas
+  }
 }
